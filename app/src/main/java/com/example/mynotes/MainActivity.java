263e1d8ef25b346b8331c37c68d325ada96b31c2
Ekaterina <yeldog@mail.ru>
2021-06-08 02:29:17 +0300

    dialogWith3Buttoms

--- app/src/main/java/com/example/mynotes/MainActivity.java
+++ app/src/main/java/com/example/mynotes/MainActivity.java
@@ -24,8 +24,30 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "OK", Toast.LENGTH_SHORT).show();
                 dialog.dismiss(); //отменили после нажатия на кнопку. Тут необязательно писать. При позитивной кнопке
                 // само сработает, а это закрытие для кастомной кнопки.
-            })
+            })       //просто смотрим, ок или отмена засчиталась
+                    .setOnCancelListener(dialog -> {
+                        Toast.makeText(this, "Cancel", Toast.LENGTH_SHORT).show();
+                    })
+                    .setOnDismissListener(dialog -> {
+                        Toast.makeText(this, "Dismiss", Toast.LENGTH_SHORT).show();
+                    })
             .show(); //show сам вызовет сначала create
+        });
+
+        findViewById(R.id.alertDialog3).setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle(R.string.title).setMessage(R.string.message3)
+                    .setCancelable(true)
+                    .setPositiveButton(R.string.ok, (dialog, which) -> {
+                        Toast.makeText(this, "Правильно", Toast.LENGTH_SHORT).show();
+                    })
+                    .setNegativeButton(R.string.no, (dialog, which) -> {
+                        Toast.makeText(this, "Не правильно", Toast.LENGTH_SHORT).show();
+                    })
+                    .setNeutralButton(R.string.dk, (dialog, which) -> {
+                        Toast.makeText(this, "Не знаю", Toast.LENGTH_SHORT).show();
+                    })
+                    .show();
         });
     }
 }
