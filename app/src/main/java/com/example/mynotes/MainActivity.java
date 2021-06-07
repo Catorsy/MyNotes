cab6052494b0660635a951ce67909bada548b25a
Ekaterina <yeldog@mail.ru>
2021-06-08 02:11:43 +0300

    alertPositiveButton

--- app/src/main/java/com/example/mynotes/MainActivity.java
+++ app/src/main/java/com/example/mynotes/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.mynotes;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.alertDialog).setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            //возвращает this, поэтому можно цепочку
+            builder.setTitle(R.string.title).setMessage(R.string.message).setIcon(R.mipmap.ic_launcher)
+            .setCancelable(false) //говорит о том, можно ли отменить наш диалог, тыкнув на серую зону
+            .setPositiveButton(R.string.ok, (dialog, which) -> { //положительная кнопка находится справа
+                Toast.makeText(this, "OK", Toast.LENGTH_SHORT).show();
+                dialog.dismiss(); //отменили после нажатия на кнопку. Тут необязательно писать. При позитивной кнопке
+                // само сработает, а это закрытие для кастомной кнопки.
+            })
+            .show(); //show сам вызовет сначала create
+        });
     }
 }
